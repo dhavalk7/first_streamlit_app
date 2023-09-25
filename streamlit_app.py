2c06a7d5681afcfcdccaa0c634b1e7a206b806f1
dhavalk7 <145999856+dhavalk7@users.noreply.github.com>
2023-09-25 08:14:04 -0600

    Update streamlit_app.py
    removed the items line

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,4 @@ streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.title('Breakfast Menu')
 
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-streamlit.title('Kale, Spinach & Rocket Smoothie')
-streamlit.title('Hard-Boiled Free-Range Egg')
+
