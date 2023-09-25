a449e6a39892a24bd573aa893cc4165e4b172869
dhavalk7 <145999856+dhavalk7@users.noreply.github.com>
2023-09-25 08:10:38 -0600

    Update streamlit_app.py
    fixing the breakfast menu line

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-Breakfast Menu
+streamlit.subtitle('Breakfast Menu')
 Omega 3 & Blueberry Oatmeal
 Kale, Spinach & Rocket Smoothie
 Hard-Boiled Free-Range Egg
